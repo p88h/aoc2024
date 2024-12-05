7c49fb80e41b0c5871ba72d8a289e1da23c3cfdc
Staszek Pasko <staszek@gmail.com>
2024-12-05 13:18:24 +0100

    total per-day and all-day times

--- BENCHMARKS.md
+++ BENCHMARKS.md
@@ -6,10 +6,13 @@ Zig: `0.14.0-dev.1911+3bf89f55c`
 
 ```
 $ zig build -Doptimize=ReleaseFast run -- all
-        parse   part1   part2
-day 01: 8.2 µs  16.9 µs 5.7 µs
-day 02: 12.4 µs 5.7 µs  17.5 µs
-day 03: 15.0 ns 14.6 µs 12.9 µs
-day 04: 9.0 ns  54.5 µs 20.4 µs
-day 05: 17.8 µs 24.7 µs 42.2 µs
+        parse   part1   part2   total
+day 00: 8.0 ns  12.0 ns 12.0 ns 33.0 ns
+day 01: 8.1 µs  17.0 µs 7.2 µs  32.5 µs
+day 02: 10.6 µs 5.6 µs  17.5 µs 33.8 µs
+day 03: 8.0 ns  15.0 µs 12.9 µs 28.0 µs
+day 04: 9.0 ns  53.3 µs 25.8 µs 79.1 µs
+day 05: 22.5 µs 21.5 µs 41.1 µs 85.2 µs
+
+all days total:         258.8 µs
 ```
--- runner.zig
+++ runner.zig
@@ -3,12 +3,12 @@ const common = @import("src/common.zig");
 const days = @import("src/_days.zig").Days;
 const Allocator = std.mem.Allocator;
 
-pub fn print_time(t: u64) void {
+fn print_time_(t: u64, fmax: comptime_int) void {
     const units = [_][]const u8{ "ns", "µs", "ms" };
     var ui: usize = 0;
     var d = t;
     var r: u64 = 0;
-    while (d > 99) {
+    while (d > fmax) {
         r = (d % 1000) / 100;
         d = d / 1000;
         ui += 1;
@@ -16,7 +16,11 @@ pub fn print_time(t: u64) void {
     std.debug.print("\t{d}.{d} {s}", .{ d, r, units[ui] });
 }
 
-pub fn run_day(allocator: Allocator, work: common.Worker) void {
+fn print_time(t: u64) void {
+    return print_time_(t, 99);
+}
+
+pub fn run_day(allocator: Allocator, work: common.Worker) u64 {
     std.debug.print("day {s}:", .{work.day});
     const filename = std.fmt.allocPrint(allocator, "input/day{s}.txt", .{work.day}) catch unreachable;
     const buf = common.read_file(allocator, filename);
@@ -25,14 +29,19 @@ pub fn run_day(allocator: Allocator, work: common.Worker) void {
     const iter = 100000;
     var ctxs = allocator.alloc(*anyopaque, iter) catch unreachable;
     for (0..iter) |i| ctxs[i] = work.parse(allocator, buf, lines);
-    print_time(t.read() / iter);
+    const t1 = t.read();
+    print_time(t1 / iter);
     t.reset();
     for (0..iter) |i| _ = work.part1(ctxs[i]);
-    print_time(t.read() / iter);
+    const t2 = t.read();
+    print_time(t2 / iter);
     t.reset();
     for (0..iter) |i| _ = work.part2(ctxs[i]);
-    print_time(t.read() / iter);
+    const t3 = t.read();
+    print_time(t3 / iter);
+    print_time((t1 + t2 + t3) / iter);
     std.debug.print("\n", .{});
+    return ((t1 + t2 + t3) / iter);
 }
 
 pub fn main() !void {
@@ -48,10 +57,14 @@ pub fn main() !void {
         if (std.mem.eql(u8, arg, "all")) runAll = true;
         if (std.mem.eql(u8, arg, "bench")) runBench = true;
     }
-    std.debug.print("\tparse\tpart1\tpart2\n", .{});
+    std.debug.print("\tparse\tpart1\tpart2\ttotal\n", .{});
     if (runAll) {
-        for (days.all) |work| run_day(allocator, work);
+        var all: u64 = 0;
+        for (days.all) |work| all += run_day(allocator, work);
+        std.debug.print("\nall days total: ", .{});
+        print_time_(all, 999);
+        std.debug.print("\n", .{});
     } else {
-        run_day(allocator, days.last);
+        _ = run_day(allocator, days.last);
     }
 }
