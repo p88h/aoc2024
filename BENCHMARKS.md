637e5c8211cdf0b0fbbcf00b6a8d6d0505ba1a8a
Staszek Pasko <staszek@gmail.com>
2024-12-07 20:59:42 +0100

    benchmark overhaul

--- BENCHMARKS.md
+++ BENCHMARKS.md
@@ -7,16 +7,16 @@ Zig: `0.14.0-dev.1911+3bf89f55c`
 ```
 $ zig build -Doptimize=ReleaseFast run -- all
         parse   part1   part2   total
-day 00: 16.0 ns 19.0 ns 18.0 ns 54.0 ns
-day 01: 8.2 µs  21.9 µs 7.2 µs  37.5 µs
-day 02: 11.6 µs 5.6 µs  17.4 µs 34.7 µs
-day 03: 18.0 ns 19.1 µs 16.2 µs 35.3 µs
-day 04: 17.0 ns 53.3 µs 18.7 µs 72.1 µs
-day 05: 13.2 µs 1.5 µs  2.5 µs  17.3 µs
-day 06: 91.0 ns 10.6 µs 10.9 µs 21.7 µs
-day 07: 45.9 µs 75.3 µs 75.9 µs 0.1 ms
+day 00: 10.0 ns 17.0 ns 17.0 ns 44.0 ns (+-2%) iter=98110    
+day 01: 8.3 µs  23.8 µs 8.1 µss 40.1 µs (+-1%) iter=9110    
+day 02: 10.9 µs 5.6 µs  17.6 µs 34.9 µs (+-1%) iter=9110    
+day 03: 10.0 ns 18.8 µs 17.2 µs 35.7 µs (+-1%) iter=9110    
+day 04: 12.0 ns 55.0 µs 21.2 µs 76.2 µs (+-1%) iter=59110    
+day 05: 13.8 µs 1.1 µs  2.5 µs  17.4 µs (+-3%) iter=98110    
+day 06: 0.1 µs  16.7 µs 1.4 ms  1.4 ms (+-1%) iter=160    
+day 07: 50.1 µs 79.3 µs 81.1 µs 0.2 ms (+-1%) iter=2010    
 
-all days total:         416.0 µs
+all days total:         1.8 ms
 ```
 
 CPU: Apple M3 Max (12+4 cores)
@@ -26,15 +26,5 @@ OS: Sonoma 14.7.1
 Zig: `0.13.0`
 
 ```
-        parse   part1   part2   total
-day 00: 7.0 ns  14.0 ns 16.0 ns 39.0 ns
-day 01: 7.9 µs  15.1 µs 6.8 µs  29.9 µs
-day 02: 11.5 µs 1.3 µs  4.6 µs  17.4 µs
-day 03: 8.0 ns  22.8 µs 19.7 µs 42.6 µs
-day 04: 7.0 ns  28.7 µs 11.7 µs 40.4 µs
-day 05: 14.0 µs 1.8 µs  2.9 µs  18.7 µs
-day 06: 0.1 µs  10.6 µs 10.4 µs 21.1 µs
-day 07: 24.5 µs 59.0 µs 44.8 µs 0.1 ms
-
-all days total:         299.0 µs
+TBD
 ```
--- runner.zig
+++ runner.zig
@@ -20,32 +20,67 @@ fn print_time(t: u64) void {
     return print_time_(t, 99);
 }
 
+fn cmpByLast(ctx: void, a: @Vector(4, u64), b: @Vector(4, u64)) bool {
+    return std.sort.asc(u64)(ctx, a[3], b[3]);
+}
+
 pub fn run_day(allocator: Allocator, work: common.Worker) u64 {
-    std.debug.print("day {s}:", .{work.day});
     const filename = std.fmt.allocPrint(allocator, "input/day{s}.txt", .{work.day}) catch unreachable;
     const buf = common.read_file(allocator, filename);
-    const lines = common.split_lines(allocator, buf);
-    var t = std.time.Timer.start() catch unreachable;
-    var task_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer task_arena.deinit();
-    var thr_arena = std.heap.ThreadSafeAllocator{ .child_allocator = task_arena.allocator() };
-    const task_allocator = thr_arena.allocator();
-    const iter = 100000;
-    var ctxs = allocator.alloc(*anyopaque, iter) catch unreachable;
-    for (0..iter) |i| ctxs[i] = work.parse(task_allocator, buf, lines);
-    const t1 = t.read();
-    print_time(t1 / iter);
-    t.reset();
-    for (0..iter) |i| _ = work.part1(ctxs[i]);
-    const t2 = t.read();
-    print_time(t2 / iter);
-    t.reset();
-    for (0..iter) |i| _ = work.part2(ctxs[i]);
-    const t3 = t.read();
-    print_time(t3 / iter);
-    print_time((t1 + t2 + t3) / iter);
+    const max_chunks = 100;
+    var times = [_]@Vector(4, u64){@splat(0)} ** max_chunks;
+    var mid: usize = 0;
+    var total_iter: usize = 0;
+    var chunk_iter: usize = 10;
+    for (0..100) |cnk| {
+        std.debug.print("\rday {s}:", .{work.day});
+        var task_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+        defer task_arena.deinit();
+        var thr_arena = std.heap.ThreadSafeAllocator{ .child_allocator = task_arena.allocator() };
+        const task_allocator = thr_arena.allocator();
+        var ctxs = task_allocator.alloc(*anyopaque, chunk_iter) catch unreachable;
+        const lines = common.split_lines(task_allocator, buf);
+        const bufs: [][]u8 = task_allocator.alloc([]u8, chunk_iter) catch unreachable;
+        for (0..chunk_iter) |i| {
+            bufs[i] = task_allocator.alloc(u8, buf.len) catch unreachable;
+            @memcpy(bufs[i], buf);
+        }
+        var t = std.time.Timer.start() catch unreachable;
+        for (0..chunk_iter) |i| ctxs[i] = work.parse(task_allocator, bufs[i], lines);
+        times[cnk][0] = t.read() / chunk_iter;
+        print_time(times[cnk][0]);
+        t.reset();
+        var a1: []u8 = undefined;
+        var a2: []u8 = undefined;
+
+        for (0..chunk_iter) |i| a1 = work.part1(ctxs[i]);
+        times[cnk][1] = t.read() / chunk_iter;
+        print_time(times[cnk][1]);
+        t.reset();
+        for (0..chunk_iter) |i| a2 = work.part2(ctxs[i]);
+        times[cnk][2] = t.read() / chunk_iter;
+        print_time(times[cnk][2]);
+        times[cnk][3] = @reduce(.Add, times[cnk]);
+        total_iter += chunk_iter;
+        if (cnk >= 10) {
+            std.mem.sort(@Vector(4, u64), times[0..cnk], {}, cmpByLast);
+            const ofs = cnk / 5;
+            const tmin = times[ofs][3];
+            const tmax = times[cnk - ofs][3];
+            const delta = 100 * (tmax - tmin) / (tmax + tmin);
+            mid = cnk / 2;
+            print_time(times[mid][3]);
+            std.debug.print(" (+-{d}%) iter={d}    ", .{ delta, total_iter });
+            if (delta <= 1) break;
+        } else {
+            print_time(times[cnk][3]);
+            std.debug.print(" (...{d}) iter={d}    ", .{ 9 - cnk, total_iter });
+        }
+        if (chunk_iter < 1000 and times[0][3] * chunk_iter < 10000000) chunk_iter *= 10;
+        //std.debug.print("    p1:[{s}] p2:[{s}]      ", .{ a1, a2 });
+    }
     std.debug.print("\n", .{});
-    return ((t1 + t2 + t3) / iter);
+    return times[mid][3];
 }
 
 pub fn main() !void {
@@ -56,11 +91,17 @@ pub fn main() !void {
     defer std.process.argsFree(allocator, args);
     var runAll = false;
     var runBench = false;
+    var day: usize = days.all.len - 1;
     common.ensure_pool(allocator);
 
     for (args) |arg| {
-        if (std.mem.eql(u8, arg, "all")) runAll = true;
-        if (std.mem.eql(u8, arg, "bench")) runBench = true;
+        if (std.mem.eql(u8, arg, "all")) {
+            runAll = true;
+        } else if (std.mem.eql(u8, arg, "bench")) {
+            runBench = true;
+        } else {
+            day = std.fmt.parseInt(usize, arg, 10) catch days.all.len - 1;
+        }
     }
     std.debug.print("\tparse\tpart1\tpart2\ttotal\n", .{});
     if (runAll) {
@@ -70,6 +111,6 @@ pub fn main() !void {
         print_time_(all, 999);
         std.debug.print("\n", .{});
     } else {
-        _ = run_day(allocator, days.last);
+        _ = run_day(allocator, days.all[day]);
     }
 }
