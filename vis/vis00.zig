d52ceed3a76e013638cb4c87693167f85248df20
Staszek Pasko <staszek@gmail.com>
2024-12-08 09:40:43 +0100

    refine base vis

--- vis/vis00.zig
+++ vis/vis00.zig
@@ -8,15 +8,44 @@ const ray = @cImport({
 });
 const sol = @import("src").day00;
 
-pub fn init(allocator: Allocator, _: *ASCIIRay) *anyopaque {
-    return common.create_ctx(allocator, sol.work);
+const VisState = struct {
+    ctx: *sol.Context,
+    posx: c_int,
+    posy: c_int,
+    dx: c_int,
+    dy: c_int,
+};
+
+pub fn init(allocator: Allocator, _: *ASCIIRay) *VisState {
+    var vis = allocator.create(VisState) catch unreachable;
+    const ptr = common.create_ctx(allocator, sol.work);
+    vis.ctx = @alignCast(@ptrCast(ptr));
+    vis.posx = 0;
+    vis.posy = 0;
+    vis.dx = 1;
+    vis.dy = 1;
+    return vis;
 }
 
-pub fn step(ptr: *anyopaque, a: *ASCIIRay, idx: usize) bool {
-    const ctx: *sol.Context = @alignCast(@ptrCast(ptr));
+pub fn step(vis: *VisState, a: *ASCIIRay, idx: usize) bool {
+    const ctx = vis.ctx;
     if (idx > 100) return true;
-    a.writeln(ctx.lines[0]);
+    a.writeXY(ctx.lines[0], vis.posx, vis.posy, ray.RAYWHITE);
+    vis.posx += vis.dx;
+    vis.posy += vis.dy;
+    if (vis.posx < 0 or vis.posx >= 120) {
+        vis.dx = -vis.dx;
+        vis.posx += vis.dx;
+    }
+    if (vis.posy < 0 or vis.posy >= 33) {
+        vis.dy = -vis.dy;
+        vis.posy += vis.dy;
+    }
     return false;
 }
 
-pub const handle = handler{ .init = init, .step = step };
+pub const handle = handler{
+    .init = @ptrCast(&init),
+    .step = @ptrCast(&step),
+    .window = .{ .fsize = 32, .fps = 30 },
+};
