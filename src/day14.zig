b6d6a23b7ee56688e9ae703628c7ca3b375d22fd
Staszek Pasko <staszek@gmail.com>
2024-12-14 10:12:02 +0100

    day 14 both parts

---
+++ src/day14.zig
@@ -0,0 +1,155 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const common = @import("common.zig");
+
+pub const vec2 = @Vector(2, i32);
+
+pub const Robot = struct {
+    pos: vec2,
+    dir: vec2,
+    pub fn init(vec: @Vector(4, i32)) Robot {
+        return Robot{ .pos = .{ vec[0], vec[1] }, .dir = .{ vec[2], vec[3] } };
+    }
+};
+
+pub fn parseVec(line: []const u8, ofs: comptime_int) @Vector(4, i32) {
+    var v: @Vector(4, i32) = @splat(0);
+    var p: usize = 0;
+    var s: i32 = 1;
+    for (ofs..line.len) |i| {
+        if (line[i] == '-') {
+            s = -1;
+        } else if (line[i] >= '0' and line[i] <= '9') {
+            v[p] = v[p] * 10 + line[i] - '0';
+        } else if (line[i - 1] >= '0' and line[i - 1] <= '9') {
+            v[p] *= s;
+            p += 1;
+            s = 1;
+        }
+    }
+    v[3] *= s;
+    return v;
+}
+
+pub const Context = struct {
+    allocator: Allocator,
+    robots: []Robot,
+    model: []u16,
+    egg: usize,
+    wait_group: std.Thread.WaitGroup,
+    running: std.atomic.Value(usize),
+};
+
+// build our 3x3 pattern detector -- this actually happens at compile time
+// .. which is nice for runtime, bad for overall compile time. but meh.
+pub fn train(ctx: *Context) void {
+    ctx.model = ctx.allocator.alloc(u16, 1 << 9) catch unreachable;
+    @memset(ctx.model, 0);
+    const patterns = [_]@Vector(2, u16){
+        .{ 0b111_111_000, 0b111_000_000 }, // horizontal edge
+        .{ 0b111_111_111, 0b000_111_000 },
+        .{ 0b000_111_111, 0b000_000_111 },
+        .{ 0b111_111_000, 0b111_111_000 }, // horizontal thick line (+full block bonus)
+        .{ 0b000_111_111, 0b000_111_111 },
+        .{ 0b110_110_110, 0b100_100_100 }, // vertical edge
+        .{ 0b111_111_111, 0b010_010_010 },
+        .{ 0b011_011_011, 0b001_001_001 },
+        .{ 0b110_110_110, 0b110_110_110 }, // vertical thick line (+also full block bonus)
+        .{ 0b011_011_011, 0b011_011_011 },
+        .{ 0b110_111_011, 0b100_010_001 }, // diagonal right
+        .{ 0b011_111_110, 0b001_010_100 }, // diagonal left
+        .{ 0b111_111_110, 0b111_110_100 }, // top left corner (= block with 1 pixel missing)
+        .{ 0b111_111_011, 0b111_011_001 }, // top right corner
+        .{ 0b011_111_111, 0b001_011_111 }, // bottom right corner
+        .{ 0b110_111_111, 0b100_110_111 }, // bottom left corner
+        .{ 0b111_111_111, 0b111_111_111 }, // full block
+    };
+    // build the scoring table
+    for (0..1 << 9) |f| {
+        for (patterns) |p| {
+            if (f & p[0] == p[1]) ctx.model[f] += 1;
+        }
+    }
+}
+
+pub const W = 101;
+pub const H = 103;
+
+pub fn parse(allocator: Allocator, _: []u8, lines: [][]const u8) *Context {
+    var ctx = allocator.create(Context) catch unreachable;
+    ctx.robots = allocator.alloc(Robot, lines.len) catch unreachable;
+    for (lines, 0..) |line, i| ctx.robots[i] = Robot.init(parseVec(line, 2));
+    ctx.allocator = allocator;
+    train(ctx);
+    ctx.egg = 0;
+    ctx.running.store(0, .seq_cst);
+    return ctx;
+}
+
+pub fn part1(ctx: *Context) []u8 {
+    var tmp: @Vector(4, i32) = @splat(0);
+    for (ctx.robots) |robot| {
+        const fpos = robot.pos + @as(vec2, @splat(100)) * robot.dir;
+        const fx = @mod(fpos[0], W);
+        const fy = @mod(fpos[1], H);
+        // std.debug.print("{any} => {any} == {d} (mod {}) {d} (mod {})\n", .{ robot, fpos, fx, W, fy, H });
+        // mid lanes
+        if (fx == W / 2 or fy == H / 2) continue;
+        // quadrants
+        if (fx < W / 2 and fy < H / 2) {
+            tmp[0] += 1;
+        } else if (fx < W / 2 and fy > H / 2) {
+            tmp[1] += 1;
+        } else if (fx > W / 2 and fy < H / 2) {
+            tmp[2] += 1;
+        } else {
+            tmp[3] += 1;
+        }
+    }
+    const ret = @reduce(.Mul, tmp);
+    return std.fmt.allocPrint(ctx.allocator, "{d}", .{ret}) catch unreachable;
+}
+
+pub fn score(ctx: *Context, fmin: usize, fmax: usize) void {
+    var frame = [_]u16{0} ** ((W * H + 8) / 9);
+    for (fmin..fmax) |f| {
+        @memset(&frame, 0);
+        for (ctx.robots) |robot| {
+            const fpos = robot.pos + @as(vec2, @splat(@intCast(f))) * robot.dir;
+            const fx = @mod(fpos[0], W);
+            const fy = @mod(fpos[1], H);
+            const idx: usize = @intCast(fy * W + fx);
+            const off = idx / 9;
+            const bit = idx % 9;
+            frame[off] |= @as(u16, 1) << @as(u4, @intCast(bit));
+        }
+        var tot: u16 = 0;
+        for (frame) |word| tot += ctx.model[word];
+        if (tot > 50) ctx.egg = f;
+        if (ctx.egg > 0) break;
+    }
+    ctx.wait_group.finish();
+}
+
+pub fn part2(ctx: *Context) []u8 {
+    ctx.wait_group.reset();
+    // look through up to 12000 frames in parallel, 1000 blocks each thread
+    for (0..12) |f| {
+        ctx.wait_group.start();
+        common.pool.spawn(score, .{ ctx, f * 1000, (f + 1) * 1000 }) catch unreachable;
+    }
+    // wait for remaining threads
+    common.pool.waitAndWork(&ctx.wait_group);
+    return std.fmt.allocPrint(ctx.allocator, "{any}", .{ctx.egg}) catch unreachable;
+}
+
+// boilerplate
+pub const work = common.Worker{
+    .day = "14",
+    .parse = @ptrCast(&parse),
+    .part1 = @ptrCast(&part1),
+    .part2 = @ptrCast(&part2),
+};
+pub fn main() void {
+    common.run_day(work);
+}
